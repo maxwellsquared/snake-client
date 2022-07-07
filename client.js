b914b0706612a75ce7b23e1df9333a2cc4476ff8
max k <maxkuhn@gmail.com>
2022-07-06 19:30:39 -0700

    Finished 'Move Commands'

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
 const net = require("net");
+const { setTimeout } = require("timers/promises");
+
 
 // establishes a connection with the game server
 const connect = function () {
@@ -9,7 +11,14 @@ const connect = function () {
 
   conn.on("connect", () => {
     console.log("Mamma mia! Luigi connect-a to da server!!!");
-    conn.write("Name: BOB");
+    conn.write("Name: GRU");
+    // for (let i = 0; i < 15; i++) {
+    //   const myMove = setTimeout(() => {conn.write("Move: up")}, (i * 500));
+    // }
+    // conn.write("Move: up");
+
+    // setInterval(() => conn.write("Move: up"), 200);
+
   })
 
   conn.on('data', (data) => {
@@ -23,3 +32,9 @@ const connect = function () {
 };
 
 module.exports = { connect };
+
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
