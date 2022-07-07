ddddeb6db908583af6c217c9f2680b0c5802681c
max k <maxkuhn@gmail.com>
2022-07-06 19:43:15 -0700

    Broke out the input functions to input.js

---
+++ input.js
@@ -0,0 +1,20 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // your code here
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === '') {
+    conn.write("Move: up");
+  }
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,25 +1,7 @@
 const { connect } = require("./client");
+const setupInput = require("./input");
 
 // setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // your code here
-  if (key === '\u0003') {
-    process.exit();
-  }
-  if (key === '') {
-    conn.write("Move: up");
-  }
-};
 
 
 console.log("Connecting ...");
